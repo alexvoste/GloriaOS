8a4441fea836807461db6f83f7a6cbd0acb6b524
alexvoste <meshoff@proton.me>
2026-05-29 19:22:51 +0000

    delete trash

--- arch/x86_64/drivers/cpuid.c
+++
@@ -1,6 +0,0 @@
-#include "../include/types.h"
-
-void cpuid(u32 code, u32* eax, u32* ebx, u32* ecx, u32* edx)
-{
-    __asm__ volatile("cpuid" : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx) : "a"(code));
-}
