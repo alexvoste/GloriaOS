32d3e46e6e4d6c6a6e430f7495208a41dcb6ec14
alexvoste <meshoff@proton.me>
2026-05-29 16:28:59 +0000

    shell: Implement strcmp, command parser, 'help', 'clear', and hardware 'reboot'

--- arch/x86_64/render-engine/put-char.asm
+++ arch/x86_64/render-engine/put-char.asm
@@ -1,11 +1,13 @@
 bits 64
+default rel
+
 global put_char_64
+global current_cursor_x
+global current_cursor_y
 
 extern send_debug_char_64
 
 section .data
-global current_cursor_x
-global current_cursor_y
 current_cursor_x: db 0
 current_cursor_y: db 0
 
@@ -25,73 +27,37 @@ put_char_64:
     jne .not_newline
     mov byte [rel current_cursor_x], 0
     inc byte [rel current_cursor_y]
-    cmp byte [rel current_cursor_y], 25
-    jl .newline_done
-    mov byte [rel current_cursor_y], 0
-.newline_done:
-    jmp .done_pop
+    call check_scroll
+    jmp .done
+    
 .not_newline:
-
-    movzx rbx, byte [current_cursor_x]
-    movzx rcx, byte [current_cursor_y]
-
-    mov rax, rcx
-    mov rdx, 80
-    mul rdx
-    add rax, rbx
-    shl rax, 1
+    cmp r12b, 0x08
+    jne .not_backspace
+    cmp byte [rel current_cursor_x], 0
+    je .done
+    dec byte [rel current_cursor_x]
     
-    mov rdi, 0xB8000
-    add rdi, rax
-    mov rsi, rdi
+    call get_screen_offset
+    mov rdi, rax
+    mov word [rdi], 0x0F20
+    jmp .done
 
-    movzx edi, r12b
-    call send_debug_char_64
-    
-    mov edi, ':'
-    call send_debug_char_64
-
-    mov rbx, rsi
-    mov rcx, 8
-.hex_push_loop:
-    mov rdx, rbx
-    and rdx, 0xF
-    cmp rdx, 9
-    jg .hex_letter
-    add dl, '0'
-    jmp .hex_pushed
-.hex_letter:
-    add dl, 0x37
-.hex_pushed:
-    push rdx
-    shr rbx, 4
-    loop .hex_push_loop
-
-    mov rcx, 8
-.hex_pop_loop:
-    pop rdx
-    movzx edi, dl
-    call send_debug_char_64
-    loop .hex_pop_loop
-
-    mov edi, 10
-    call send_debug_char_64
-
-    mov rdi, rsi
+.not_backspace:
+    call get_screen_offset
+    mov rdi, rax
     mov al, r12b
     mov ah, 0x0F
     mov [rdi], ax
 
-    inc byte [current_cursor_x]
-    cmp byte [current_cursor_x], 80
+    inc byte [rel current_cursor_x]
+    cmp byte [rel current_cursor_x], 80
     jl .done
-    mov byte [current_cursor_x], 0
-    inc byte [current_cursor_y]
-    cmp byte [current_cursor_y], 25
-    jl .done
-    mov byte [current_cursor_y], 0
+    mov byte [rel current_cursor_x], 0
+    inc byte [rel current_cursor_y]
+    call check_scroll
 
 .done:
+    call update_hardware_cursor
     pop r12
     pop rsi
     pop rdi
@@ -101,12 +67,62 @@ put_char_64:
     pop rax
     ret
 
-.done_pop:
-    pop r12
-    pop rsi
-    pop rdi
+get_screen_offset:
+    movzx rbx, byte [rel current_cursor_x]
+    movzx rcx, byte [rel current_cursor_y]
+    mov rax, rcx
+    mov rdx, 80
+    mul rdx
+    add rax, rbx
+    shl rax, 1
+    add rax, 0xB8000
+    ret
+
+check_scroll:
+cmp byte [rel current_cursor_y], 25
+    jl .no_scroll
+
+    mov rsi, 0xB8000 + 160
+    mov rdi, 0xB8000
+    mov rcx, 480
+    cld
+    rep movsq
+
+    mov rdi, 0xB8000 + 3840
+    mov ax, 0x0F20
+    mov rcx, 80
+    rep stosw
+
+    mov byte [rel current_cursor_y], 24
+.no_scroll:
+    ret
+
+update_hardware_cursor:
+    push rax
+    push rbx
+    push rdx
+
+    movzx rax, byte [rel current_cursor_y]
+    mov rdx, 80
+    mul rdx
+    movzx rbx, byte [rel current_cursor_x]
+    add rbx, rax
+
+    mov dx, 0x3D4
+    mov al, 14
+    out dx, al
+    mov dx, 0x3D5
+    mov al, bh
+    out dx, al
+
+    mov dx, 0x3D4
+    mov al, 15
+    out dx, al
+    mov dx, 0x3D5
+    mov al, bl
+    out dx, al
+
     pop rdx
-    pop rcx
     pop rbx
     pop rax
     ret
--- arch/x86_64/user/shell.asm
+++ arch/x86_64/user/shell.asm
@@ -3,8 +3,17 @@ default rel
 %include "arch/x86_64/system/opcodes.inc"
 
 section .data
-prompt: db '> ', 0
-welcome: db 'GloriaOS Interactive Shell. Write something!', 0x0A, 0
+prompt: db 0x0A, '> ', 0
+welcome: db 'GloriaOS Interactive Shell v1.0. Ready.', 0x0A, 0
+help_text: db 'Available commands:', 0x0A, '  help   - Show this help', 0x0A, '  clear  - Clear the screen', 0x0A, '  reboot - Hard reboot the system', 0x0A, 0
+unknown_cmd: db 'Unknown command. Type "help" for a list of commands.', 0x0A, 0
+
+cmd_help: db 'help', 0
+cmd_clear: db 'clear', 0
+cmd_reboot: db 'reboot', 0
+
+section .bss
+cmd_buffer: resb 64
 
 section .text
 global shell_entry_point
@@ -13,13 +22,13 @@ extern put_char_64
 extern keyboard_read_char
 
 shell_entry_point:
+    call do_clear
     lea rdi, [rel welcome]
     call print_string_64
 
 .loop_prompt:
     lea rdi, [rel prompt]
     call print_string_64
-
     xor r12, r12
 
 .read_loop:
@@ -31,32 +40,127 @@ shell_entry_point:
     cmp al, 0x08
     je .handle_backspace
 
-    cmp r12, 63
+    cmp r12, 62
     jge .read_loop
+
+    lea rbx, [rel cmd_buffer]
+    mov [rbx + r12], al
+    inc r12
 
     movzx rdi, al
     call put_char_64
-    inc r12
     jmp .read_loop
 
 .handle_backspace:
     cmp r12, 0
     je .read_loop
     dec r12
-    extern current_cursor_x
-    dec byte [rel current_cursor_x]
-    mov rdi, ' '
+
+    lea rbx, [rel cmd_buffer]
+    mov byte [rbx + r12], 0
+
+    mov rdi, 0x08
     call put_char_64
-    dec byte [rel_cursor_re_dec]
     jmp .read_loop
 
 .handle_enter:
+    lea rbx, [rel cmd_buffer]
+    mov byte [rbx + r12], 0
+
     mov rdi, 0x0A
     call put_char_64
-    
+
+    cmp r12, 0
+    je .loop_prompt
+
+    lea rdi, [rel cmd_buffer]
+    lea rsi, [rel cmd_help]
+    call strcmp
+    test rax, rax
+    jz .exec_help
+
+    lea rdi, [rel cmd_buffer]
+    lea rsi, [rel cmd_clear]
+    call strcmp
+    test rax, rax
+    jz .exec_clear
+
+    lea rdi, [rel cmd_buffer]
+    lea rsi, [rel cmd_reboot]
+    call strcmp
+    test rax, rax
+    jz .exec_reboot
+
+    lea rdi, [rel unknown_cmd]
+    call print_string_64
     jmp .loop_prompt
 
-section .text
-rel_cursor_re_dec:
-    dec byte [rel current_cursor_x]
+.exec_help:
+    lea rdi, [rel help_text]
+    call print_string_64
+    jmp .loop_prompt
+
+.exec_clear:
+    call do_clear
+    jmp .loop_prompt
+
+.exec_reboot:
+    call do_reboot
+    jmp .loop_prompt
+
+strcmp:
+    xor rax, rax
+.loop:
+    mov al, [rdi]
+    mov bl, [rsi]
+    cmp al, bl
+    jne .not_equal
+    cmp al, 0
+    je .equal
+    inc rdi
+    inc rsi
+    jmp .loop
+.not_equal:
+    mov rax, 1
     ret
+.equal:
+    xor rax, rax
+    ret
+
+do_clear:
+    mov rcx, 2000
+    mov rdi, 0xB8000
+    mov ax, 0x0F20
+    rep stosw
+
+    extern current_cursor_x
+    extern current_cursor_y
+    mov byte [rel current_cursor_x], 0
+    mov byte [rel current_cursor_y], 0
+
+    mov dx, 0x3D4
+    mov al, 14
+    out dx, al
+    mov dx, 0x3D5
+    xor al, al
+    out dx, al
+    mov dx, 0x3D4
+    mov al, 15
+    out dx, al
+    mov dx, 0x3D5
+    xor al, al
+    out dx, al
+    ret
+
+do_reboot:
+.wait_1:
+    in al, 0x64
+    test al, 2
+    jnz .wait_1
+
+    mov al, 0xFE
+    out 0x64, al
+
+.halt:
+    hlt
+    jmp .halt
